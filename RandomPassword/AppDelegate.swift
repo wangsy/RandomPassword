ae41c2627d83db3b4ac084fd5b9a63413948bed0
Sooyong Wang <wangsy@wangsy.com>
2015-04-21 13:24:19 +0900

    Initial Commit

---
+++ RandomPassword/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RandomPassword
+//
+//  Created by Sooyong Wang on 2015. 4. 21..
+//  Copyright (c) 2015년 Sooyong Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
